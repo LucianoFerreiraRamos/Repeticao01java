186b132e2a4dd46b17df19d085dd53c593bc878e
Luciano <97047885+LucianoFerreiraRamos@users.noreply.github.com>
2022-02-16 18:00:20 -0300

    Repeticao01

---
+++ src/repeticao01/Repeticao01.java
@@ -0,0 +1,18 @@
+package repeticao01;
+
+public class Repeticao01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int cc = 0;
+		do {
+			System.out.println("Cambalhota");
+			cc++;
+						
+		} while(cc<4);
+		
+		
+
+	}
+
+}
